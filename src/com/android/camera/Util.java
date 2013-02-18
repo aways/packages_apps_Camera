f9b2c01d10d59699adbe7611b8cefdc1fe010fe6
Prasong Aroonruviwat <psafx@hotmail.com>
2013-02-18 18:44:07 +0700

    Fix build error

--- src/com/android/camera/Util.java
+++ src/com/android/camera/Util.java
@@ -139,6 +139,9 @@ public class Util {
     // HTC camcorder mode
     private static boolean sHTCCamMode;
 
+    // For setting video desired profile size
+    private static boolean sProfileVideoSize;
+
     // For setting video size before recording starts
     private static boolean sEarlyVideoSize;
 
@@ -163,6 +166,7 @@ public class Util {
         // These come from the config, but are needed before parameters are set.
         sSamsungCamMode = context.getResources().getBoolean(R.bool.needsSamsungCamMode);
         sHTCCamMode = context.getResources().getBoolean(R.bool.needsHTCCamMode);
+        sProfileVideoSize = context.getResources().getBoolean(R.bool.useProfileVideoSize);
         sEarlyVideoSize = context.getResources().getBoolean(R.bool.needsEarlyVideoSize);
         sEnableZSL = context.getResources().getBoolean(R.bool.enableZSL);
         sNoFaceDetectOnFrontCamera = context.getResources().getBoolean(
@@ -179,6 +183,10 @@ public class Util {
 
     public static boolean useSamsungCamMode() {
         return sSamsungCamMode;
+    }
+
+    public static boolean useProfileVideoSize() {
+        return sProfileVideoSize;
     }
 
     public static boolean needsEarlyVideoSize() {
