CyanogenMod/android_packages_apps_Camera

Camera : Timer Snapshot
Allow user to take a snapshot after a certain amount of time.
Credits : Michael Simon for the base port

Change-Id: I13b154832af07b4b5af1414b89dc0113680c402b

src/com/android/camera/CameraSettings.java
@@ -46,6 +46,7 @@ public class CameraSettings {
     public static final String KEY_PICTURE_SIZE = "pref_camera_picturesize_key";
     public static final String KEY_JPEG_QUALITY = "pref_camera_jpegquality_key";
     public static final String KEY_FOCUS_MODE = "pref_camera_focusmode_key";
+    public static final String KEY_CAPTURE_MODE = "pref_camera_timer_key";
     public static final String KEY_FLASH_MODE = "pref_camera_flashmode_key";
     public static final String KEY_VIDEOCAMERA_FLASH_MODE = "pref_camera_video_flashmode_key";
     public static final String KEY_WHITE_BALANCE = "pref_camera_whitebalance_key";

aways/packages_apps_Camera

	  David Van Tonder for indicator icon
Change-Id: Ic50bf1c1f7fd85d8c48dac3e51766aeff2b381ea

src/com/android/camera/CameraSettings.java
@@ -72,6 +72,7 @@ public class CameraSettings {
     public static final String KEY_VIDEOCAMERA_COLOR_EFFECT = "pref_camera_video_coloreffect_key";
     public static final String KEY_BURST_MODE = "pref_camera_burst_key";
     public static final String KEY_VOICE_SHUTTER = "pref_voice_shutter_key";
+    public static final String KEY_TIMER_MODE = "pref_camera_timer_key";
 
     public static final String EXPOSURE_DEFAULT_VALUE = "0";
     public static final String VALUE_ON = "on";